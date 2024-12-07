828e6374b7553e9f1946616ba9da8b05d0c7104c
Efe Can Kara <efecan_kara@proton.me>
2024-12-07 20:33:41 +0300

    Add - Day 1 (Already Solved)

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,1 +1,27 @@
-// Solution for Day1
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    int x;
+    double y;
+    string z;
+
+    int i(4);
+    double d(4.0);
+    string s("HackerRank ");
+
+    cin >> x;
+    cin >> y;
+    cin.ignore();
+    getline(cin, z);
+
+    cout << i + x << endl;
+    cout << fixed << setprecision(1) << d + y << endl;  // Set precision to 1 decimal place
+    cout << s << z << endl;
+}
