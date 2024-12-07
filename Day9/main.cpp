3a747a56e98ca3f351dbe32bd969d54f1f3fbf8a
Efe Can Kara <efecan_kara@proton.me>
2024-12-07 20:42:37 +0300

    Add - Day 9 (Already Solved)

--- Day9/main.cpp
+++ Day9/main.cpp
@@ -1,1 +1,23 @@
-// Solution for Day9
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int factorial(int a){
+    if (a > 0) {
+        return a * factorial(a -1 );
+    } else {
+        return 1;
+    }
+}
+
+
+int main() {
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int x;
+    cin >> x;
+    cout << factorial(x) << endl;
+}
