b1ffbc5093a31f4fd835c7651af4bc976d9318d4
Efe Can Kara <efecan_kara@proton.me>
2024-12-07 20:39:26 +0300

    Add - Day 6 (Already Solved)

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,1 +1,32 @@
-// Solution for Day6
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    
+    while (T--){
+        string s;
+        getline(cin, s);
+        
+        string even_string = "";
+        string odd_string = "";
+        
+        for (int i = 0; i < s.length(); i++){
+            if (i % 2 != 0){
+                even_string += s[i]; 
+            }
+            else {
+                odd_string += s[i]; 
+            }
+        }
+        
+        cout << odd_string << " " << even_string << endl;
+    }
+}
